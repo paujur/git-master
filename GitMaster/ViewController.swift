47b90038be4ef0e9ae5874cfe20cab1aa569caf4
Paul Jurczyk <pauljurczykny@gmail.com>
2016-12-05 14:29:23 -0500

    Random

--- GitMaster/ViewController.swift
+++ GitMaster/ViewController.swift
@@ -9,6 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: IBOutlets ------------------------
+    
+    @IBOutlet weak var label1: UILabel!
+    
+    @IBOutlet weak var textinput: UITextField!
+    
+    // MARK: IBActions -------------------
+    
+    @IBAction func button(_ sender: UIButton) {
+        
+        label1.text = textinput.text
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
